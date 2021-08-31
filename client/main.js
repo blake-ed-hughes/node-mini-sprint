3077ea504c758b7424b591fe30be8511382634de
Blake Hughes <blake.ed.hughes@gmail.com>
2021-08-30 21:20:37 -0500

    Step 1 (raw node) complete!

--- client/main.js
+++ client/main.js
@@ -10,15 +10,37 @@ $(document).ready(function() {
     addQuote(quote);
   });
 
-  function getQuote(){
-
+  function getQuote(successCB, errorCB = null){
+    // $.get('http://localhost:3000/quote', data => {console.log('DATA:', data)});
     //YOUR CODE HERE, Add a GET request
+    $.ajax({
+      url:'http://localhost:3000/quote',
+      type: 'GET',
+      dataType: 'json',
+      success: function (data) {
+        console.log('DATA:', data);
+      },
+      error: errorCB || function (error) {
+        console.error('Failed to create quote', error);
+      }
+    });
 
   }
 
-  function addQuote(quote){
-    
-    //YOUR CODE HERE, Add a POST request
+  function addQuote(quote, successCB, errorCB = null){
 
+    //YOUR CODE HERE, Add a POST request
+    $.ajax({
+      url: 'http://localhost:3000/quote',
+      type: 'POST',
+      data: quote,
+      dataType: 'json',
+      success: function (data) {
+        console.log('DATA:', data)
+      },
+      error: errorCB || function (error) {
+        console.error('Failed to create quote', error);
+      }
+    });
   }
 });
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+// const { parse } = require('querystring');
 
 //headers to allows CORS requests
 const headers = {
@@ -11,10 +12,10 @@ const headers = {
 const port = 3000;
 
 //Fill with strings of your favorite quotes :)
-const quotes = [
+var quotes = [
   'this is a quote',
-  'another quote i like',
-  'what even is a quote',
+  'here is another quote i like',
+  'what even is quote',
   'quote quote quote quotey quote quote',
   'i should really add some quotes later'
 ];
@@ -30,29 +31,45 @@ const handleRequest = function(req, res) {
   console.log(`Endpoint: ${req.url} Method: ${req.method}`);
 
   // redirect users to /quote if they try to hit the homepage. This should already work, no changes needed
+  // if (req.url == '/' && req.method == "OPTIONS") {
+  //   console.log('redirecting');
+  //   res.writeHead(301, {...headers, Location: `http://localhost:${port}/quote`}) //redirect to quote
+  //   res.end();
+  // }
+
   if (req.url == '/') {
     console.log('redirecting');
     res.writeHead(301, {...headers, Location: `http://localhost:${port}/quote`}) //redirect to quote
     res.end();
   }
 
-  // TODO: GET ONE
+  // TODO: GET ONE √
   if ((req.url == '/quote/' || req.url == '/quote') && req.method == "GET") {
     // Use the getRandomInt to randomly select a quote from the quote array
     var randomQuotesIndex = getRandomInt(0, quotes.length);
     // console.log('randomQuotesIndex >>>>>>>>>>>>VALUE>>>>>>>>>>>>>>>> ', randomQuotesIndex);
+    // Send status code
     res.writeHead(200, headers);
-    //Be sure to .end
+    // Be sure to .end
     res.end(JSON.stringify(quotes[randomQuotesIndex]));
   }
+
+
   // TODO: POST/CREATE
   // Paths should be the same
   else if ((req.url == '/quote/'  || req.url == '/quote') && req.method == "POST") {
-    //YOUR CODE HERE
+    // Adds a new string to the quotes array
+    req.on('data', function(chunk) {
+      quotes.push('' + chunk);
+      // console.log(quotes);
+    });
+    // Send status code
+    res.writeHead(201, headers);
+    // Be sure to .end
 
-    //Be sure to .end
-    res.end();
+    res.end(JSON.stringify(quotes));
   }
+
 
 //CATCH ALL ROUTE
   else {
